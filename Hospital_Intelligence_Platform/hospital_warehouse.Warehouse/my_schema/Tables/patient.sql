417fcf50bcae2de0cfbf4d5cb27ae0e0ab99fc55
Aniket Belsare <114948982+abelsare348@users.noreply.github.com>
2026-01-03 21:10:57 +0530

    Committing 6 items from workspace 8edd74b2-e6e6-4847-94a1-e96434b2ea84

---
+++ Hospital_Intelligence_Platform/hospital_warehouse.Warehouse/my_schema/Tables/patient.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [my_schema].[patient] (
+
+	[Patient_ID] varchar(8000) NULL, 
+	[Name] varchar(8000) NULL, 
+	[Age] varchar(8000) NULL, 
+	[Gender] varchar(8000) NULL, 
+	[Contact_Info] varchar(8000) NULL, 
+	[Medical_History] varchar(8000) NULL, 
+	[Active_Flag] varchar(8000) NULL
+);
