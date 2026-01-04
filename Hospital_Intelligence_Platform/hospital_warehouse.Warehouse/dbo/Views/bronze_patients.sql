0f70099d155b8a6d00909e036d2659d5f82332bc
Aniket Belsare <114948982+abelsare348@users.noreply.github.com>
2026-01-04 12:22:41 +0530

    Committing 3 items from workspace 8edd74b2-e6e6-4847-94a1-e96434b2ea84

---
+++ Hospital_Intelligence_Platform/hospital_warehouse.Warehouse/dbo/Views/bronze_patients.sql
@@ -0,0 +1,3 @@
+-- Auto Generated (Do not modify) ADFEFA658C56C5F9A4914C86DACB22EAA3C03476BC265DFF419FDC09982733F9
+CREATE VIEW dbo.bronze_patients AS
+SELECT * FROM [hospital_lakehouse].[dbo].[bronze_patients]
