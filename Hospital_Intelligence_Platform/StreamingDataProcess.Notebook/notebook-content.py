1a6b1ae0570ca9d9d4f6fba3fa05af46800731c5
Aniket Belsare <114948982+abelsare348@users.noreply.github.com>
2026-01-06 21:11:34 +0530

    Committing 1 item from workspace 8edd74b2-e6e6-4847-94a1-e96434b2ea84

--- Hospital_Intelligence_Platform/StreamingDataProcess.Notebook/notebook-content.py
+++ Hospital_Intelligence_Platform/StreamingDataProcess.Notebook/notebook-content.py
@@ -11,9 +11,28 @@
 
 # CELL ********************
 
-# Welcome to your new notebook
-# Type here in the cell editor to add code!
+import random as ran
+import time
 
+while True:
+    data = []
+    for i in range(0, 10, 1):
+        row = []
+        row.append(ran.randint(500, 5000))
+        row.append(ran.randint(1, 10))
+        row.append(ran.randint(1, 5))
+        row.append(ran.randint(1, 100))
+        row.append(ran.randint(100000, 999999))
+        row.append(time.strftime("%Y%m%d-%H%M%S"))
+        data.append(row)
+
+    df2 = spark.createDataFrame(data, ['patient_id', 'bedrooms', 'min_heart_rate', 'max_heart_rate', 'timestamp'])
+    timestamp_var = time.strftime("%Y%m%d-%H%M%S")
+    
+    
+
+    time.sleep(10)
+    
 
 # METADATA ********************
 
